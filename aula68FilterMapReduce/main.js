fd14c63e3dea695d4d244f888cda5a0cac0966e5
felipecabesssa <felipesoares100@hotmail.com>
2022-02-25 23:59:01 -0300

    Filter + Map + Reduce 2/2 - Usando dedo de seta

--- aula68FilterMapReduce/main.js
+++ aula68FilterMapReduce/main.js
@@ -1,6 +1,8 @@
 // Retorne a soma do dobro de todos os pares
 
-
+//filtrar pares
+//dobrar valores
+//reduzir (somar tudo)
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const numerosPares = numeros.filter(function(valor) {
     return valor % 2 === 0;
@@ -11,3 +13,12 @@ const numerosPares = numeros.filter(function(valor) {
 });
 
 console.log(numerosPares);
+
+console.log('\nUsando arrow function\n');
+
+const numerosPares2 = numeros
+.filter(valor => valor % 2 === 0)
+.map(valor => valor * 2)
+.reduce((ac, valor) => ac + valor);
+
+console.log(numerosPares2);
