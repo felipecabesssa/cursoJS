7552db48e67cb365450507dc116b0385a4103374
felipecabesssa <felipesoares100@hotmail.com>
2022-03-10 17:06:29 -0300

    Escrevendo e lendo arquivos *HOTIP*

---
+++ node/aula5LerEscreverArquivos/modules/escrever.js
@@ -0,0 +1,19 @@
+const fs = require('fs').promises;
+/* const path = require('path');
+const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt');
+fs.writeFile(caminhoArquivo, 'olá JS\n', { flag: 'a' }); */
+// Essas 4 linhas de código acima criam um arquivo teste.txt um diretório 
+// antes do que a gente ta com o '..' e escreve (fs.writeFile) 
+// Frase 1 no arquivo teste.txt
+
+// o a no lugar do w não sobrescreve o arquivo, adiciona.
+
+// e a flag w Faz com que caso ja exista o arquivo ele sobreescreve
+// Frase 1 no arquivo teste.txt
+
+
+module.exports = (caminho, dados) => {
+  fs.writeFile(caminho, dados, { flag: 'w', encoding: 'utf8' });
+};
+
+
