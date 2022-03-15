dba0e1ce0035eda0883b4e3872831c658f952a39
felipecabesssa <felipesoares100@hotmail.com>
2022-03-15 20:55:33 -0300

    Ajustes no layout da agenda

--- projetoagenda/src/controllers/contatoController.js
+++ projetoagenda/src/controllers/contatoController.js
@@ -21,7 +21,7 @@ exports.register = async (req, res) => {
         }  
 
         req.flash('success', 'Contato registrado com sucesso.');
-        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        req.session.save(() => res.redirect('/'));
         return;
     } catch(e) {
         console.log(e);
